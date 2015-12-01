45a722e4aee24d17d1e00601aa2bf397ebde3552
Your Name <you@example.com>
2015-12-01 14:30:58 -0500

    Counting Loop

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loopnumbercount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter ending number");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" ");
+
+            while (x <= y)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+
+        }
+    }
+}
